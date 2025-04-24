1b5d264651ca5a29ca22a50c43fcb865bdb84760
ilkinismayilov-905 <ismayilovilkin2006@gmail.com>
2025-04-24 19:29:40 +0400

    first commit

---
+++ src/test/java/com/example/PipelineServer/PipelineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PipelineServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
