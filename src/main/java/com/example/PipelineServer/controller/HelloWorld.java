d83fd24e3dbc3adb1fd1bc9ab04f137a3f8ad126
ilkinismayilov-905 <ismayilovilkin2006@gmail.com>
2025-04-26 10:54:50 +0400

    dev branch

--- src/main/java/com/example/PipelineServer/controller/HelloWorld.java
+++ src/main/java/com/example/PipelineServer/controller/HelloWorld.java
@@ -1,14 +1,15 @@
 package com.example.PipelineServer.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @GetMapping
-    public void getHello(){
-        System.out.println("Hello");
+    @GetMapping("/hello")
+    public ResponseEntity<?> getHello(){
+       return ResponseEntity.ok("Hello");
     }
 }
